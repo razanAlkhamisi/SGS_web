6c2fe9f39052cdb91306379df38fcee885ac6a5d
Razan Alkhamisi <124253161+razanAlkhamisi@users.noreply.github.com>
2025-04-22 10:14:34 +0300

    Update SGS_.py
    Signed-off-by: Razan Alkhamisi <124253161+razanAlkhamisi@users.noreply.github.com>

--- SGS_.py
+++ SGS_.py
@@ -2,6 +2,50 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
+repairing_in_progress = [
+    "Repairing in Progress", "Under Diagnosis/Troubleshooting", "Differential Overhaul in Progress",
+    "Engine Overhaul in Progress", "Engine OVH in progress", "Refurbishment in progress",
+    "Refurbishment in Progress", "Under Fabrication process", "Under line",
+    "Under Troubleshooting / Diagnosis", "Waiting for customer response",
+    "Work in Progress", "Under Troubleshooting /Diagnosis", "waiting for customer response"
+]
+
+tuv_pm = ["Under TUV Preparation","Preventive Maintenance in progress"]
+
+oem=[
+    "Under Warranty Waiting for Manufacturer Support",
+    "Waiting for Manufacturer Support","Waiting for OEM Support"]
+
+waiting_for_parts=[
+    "Waiting for Engine to be delivered","Engine OVH in progress | Waiting For Parts",
+    "Waiting For Parts","Waiting For Parts ( Control Valve )","Waiting for Parts to be delivered",
+    "Waiting for spare parts to be deliverd","Waiting for the part to be delivered.",
+    "Waiting part to be delivered","Wiaitng for parts to be delivered"]
+
+
+def classify_remarks(remarks_value):
+ 
+    remarks_value = str(remarks_value).strip()  # تنظيف النص وإزالة المسافات الزائدة
+    
+    for category in repairing_in_progress:
+        if category.lower() in remarks_value.lower():
+            return "Repairing in Progress"  # تعيين التصنيف المطابق
+    
+    for category in tuv_pm:
+        if category.lower() in remarks_value.lower():
+            return "TUV + PM"  # تعيين التصنيف المطابق
+
+    for category in oem:
+        if category.lower() in remarks_value.lower():
+            return "Waiting for OEM Support"  # تعيين التصنيف المطابق
+
+    for category in waiting_for_parts:
+        if category.lower() in remarks_value.lower():
+            return "Waiting for Parts"  # تعيين التصنيف المطابق
+    
+    return "N/A"  # في حال لم يكن هناك تطابق
+
+
 def main():
 
 
@@ -68,23 +112,35 @@ def main():
         unsafe_allow_html=True 
     ) 
  
+
+
     st.logo("images/sgs_logo.jpg", size="large")  # تعديل الشعار
 
     st.title("دمج البيانات")
-
+   
     # تحميل الملف الرئيسي
     main_file = st.file_uploader("(Excel) تحميل الملف الرئيسي", type=["xlsx"], key="main_file")
     if main_file:
         main_df = pd.read_excel(main_file, engine="openpyxl", header=0)
         st.write("### الملف الرئيسي")
         st.write(main_df)
-
+        
+        main_df["Remarks_1"]=classify_remarks(main_df["Remarks"])
         # تحقق من وجود عمود GS
         if "GS" not in main_df.columns:
             st.error("GS "+"الملف الرئيسي يجب أن يحتوي على عمود ")
             return
-
+   
+       
         # إضافة أعمدة جديدة إذا لم تكن موجودة
+        if "SRA" in main_df.columns:
+            main_df["SRA"]=0
+
+        
+        if "Reason NMC" in main_df.columns:
+            main_df["OOS"]=main_df["Reason NMC"]
+            main_df.drop(columns=["Reason NMC"],inplace=True)
+
         for col in ["Date", "OOS", "ST", "Remarks"]:
             if col not in main_df.columns:
                 main_df[col] = None
@@ -94,11 +150,11 @@ def main():
             "تحميل الملفات الإضافية", type=["xlsx"], accept_multiple_files=True, key="additional_files"
         )
 
+       
         if additional_files:
-            for uploaded_file in additional_files:
-                sheets = pd.read_excel(uploaded_file, engine="openpyxl", sheet_name=None, header=0)
 
-               
+            for uploaded_file in additional_files:
+                sheets = pd.read_excel(uploaded_file, engine="openpyxl", sheet_name=None, header=0,skiprows=4)
 
                     
                 valid_sheets = {}
@@ -140,44 +196,42 @@ def main():
                         for _, row in additional_df.iterrows():
                             gs_value = row[selected_column]
                             date_value = row[date_column]
+                            loc=row["Location"]
+                            loc=str(loc)[2:]
                             reason_nmc_value = row.get("Reason NMC", None)
                             remarks_value = row.get("Next Action", None)
+                            sra = "SRA" if "SRA" in uploaded_file.name else 0 
+
+                            classification= classify_remarks(remarks_value)
 
                             if gs_value in main_df["GS"].values:
                                 # تحديث الصفوف الموجودة
-                                main_df.loc[main_df["GS"] == gs_value, ["Date", "OOS", "Remarks"]] = [
-                                    date_value, reason_nmc_value, remarks_value
+                                main_df.loc[main_df["GS"] == gs_value, ["Date", "OOS", "Remarks","SRA", "Remarks_1"]] = [
+                                    date_value, reason_nmc_value, remarks_value,sra,classification
                                 ]
-
+                          
                             else:
-                                 # تحديد معرف الموقع بناءً على اسم الملف
-                                if "Jeddah" or "JED" in uploaded_file.name:
-                                    file_identifier = "JED"
-                                elif "Riyadh" or "RUH" in uploaded_file.name:
-                                    file_identifier = "RUH"
-                                elif "Medina" or "MED" in uploaded_file.name:
-                                    file_identifier = "MED"
-                                elif "Dammam" or "DMM" in uploaded_file.name:
-                                    file_identifier = "DMM"
-                                elif "DHA" or "DHA" in uploaded_file.name:
-                                    file_identifier = "DHA"
-                                elif "HOF" or "HOF" in uploaded_file.name:
-                                    file_identifier = "HOF"
-                                elif "Local Station" or "LS" in uploaded_file.name:
-                                    file_identifier = "LS"
-                                else:
-                                    file_identifier = uploaded_file.name
-
-
-
+                            
                                 # إضافة صف جديد
                                 new_row = {
                                     "GS": gs_value,
                                     "Date": date_value,
                                     "OOS": reason_nmc_value,
-                                    "ST": file_identifier,  # اسم الملف كمصدر
-                                    "Remarks": remarks_value
+                                    "ST": loc, 
+                                    "Remarks": remarks_value,
+                                    "Remarks_1":classification
                                 }
+                                if "SRA" in main_df.columns:
+                                    new_row = {
+                                    "GS": gs_value,
+                                    "Date": date_value,
+                                    "OOS": reason_nmc_value,
+                                    "ST": loc, 
+                                    "Remarks": remarks_value,
+                                    "Remarks_1":classification,
+                                    'SRA':sra
+                                }
+
                                 main_df = pd.concat([main_df, pd.DataFrame([new_row])], ignore_index=True)
 
             # عرض الملف المحدث
